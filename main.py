6bd60dabb48c9fbbdd4ca4e94f2fde60ce608c36
Juan Cruz <jc23729@gmail.com>
2020-12-29 21:06:59 +0000

    created a function game()

--- main.py
+++ main.py
@@ -13,14 +13,6 @@ def check_answer(guess, answer):
     else:
         print(f"You got it! The answer was {answer}.")
 
-
-#Step 1 Choosing a random number between 1 and 100
-
-print("Welcome to the guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-answer = randint(1, 100)
-print(f"Pssst the correct answer is {answer}")
-
 #4. Make a function to set difficulty changed order because should have the difficulty level choice first
 
 
@@ -30,22 +22,31 @@ def set_difficulty():
         return EASY_LEVEL_TURNS
     else:
         return HARD_LEVEL_TURNS
+#Created a function game
+def game():
+    #Step 1 Choosing a random number between 1 and 100
+
+  print("Welcome to the guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+  answer = randint(1, 100)
+  print(f"Pssst the correct answer is {answer}")
+
+  #2. Let the user guess a number
+  #guess = int(input("Make a guess:")) MOVED TO TRACK NUMBER OF TURNS
+  turns = set_difficulty()
+  print(f"You have {turns} attempts remaining to guess a number")
+
+  # Track the number of turns and reduce by 1 if they get it wrong
+  #Repeat the guessing functionality if they get it wrong
+  guess = 0
+  while guess != answer:
+    #Let the user guess a number
+    guess = int(input("Make a guess:"))
+
+  check_answer(guess, answer)
 
 
-#2. Let the user guess a number
-#guess = int(input("Make a guess:")) MOVED TO TRACK NUMBER OF TURNS
-turns = set_difficulty()
-print(f"You have {turns} attempts remaining to guess a number")
-
-# Track the number of turns and reduce by 1 if they get it wrong
-#Repeat the guessing functionality if they get it wrong
-guess = 0
-while guess != answer:
-  #Let the user guess a number
-  guess = int(input("Make a guess:"))
-
-check_answer(guess, answer)
-
+game()
 
 
 
