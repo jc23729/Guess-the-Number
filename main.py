c82b73431f34dd5604488926e3c1a60776d9f1e6
Juan Cruz <jc23729@gmail.com>
2020-12-29 19:44:10 +0000

    Choosing a random number from 1 and 100

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #Choosing a random number between 1 and 100
+import random
 
+print("Welcome to the guessing Game!")
+print ("I'm thinking of a number between 1 adn 100.")
+answer = randint(1, 100)
 
 #Make a function to set difficulty
+
+
 
 #Let the user guess a number
 
