03cbcbe6c22e0ff6206da0f2d099dd765a5c1984
Juan Cruz <jc23729@gmail.com>
2020-12-29 20:01:14 +0000

    Setting difficulty level

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from random import randint
+#Part of step 4 set these two constants that are Global
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 
 # 3 Function to check if user's guess against actual answer
 def check_answer(guess, answer):
@@ -19,6 +23,11 @@ answer = randint(1, 100)
 #4. Make a function to set difficulty
 def set_difficulty():
   level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  if level == "easy":
+    turns = EASY_LEVEL_TURNS
+  else:
+    turns = HARD_LEVEL_TURNS
+
 
 
 #2. Let the user guess a number
