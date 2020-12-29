a982d6dbc37617e8eb2d568e8f5019e199996b40
Juan Cruz <jc23729@gmail.com>
2020-12-29 19:41:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Choosing a random number between 1 and 100
+
+
+#Make a function to set difficulty
+
+#Let the user guess a number
+
+
+# Function to check if user's guess against actual answer
+
+
+# Track the number of turns and reduce by 1 if they get it wrong 
+
+
+
+#Repeat the guessing functionality if they get it wrong
+
+
